d060fcbc052b7cda8d58092a8c11d736655978d6
Алексей <setgecko@gmail.com>
2023-05-29 21:39:34 +0300

    Добавьте файлы проекта.

---
+++ SF.Module18/InlineProgress.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF.Module18
+{
+    internal class InlineProgress : IProgress<double>, IDisposable
+    {
+        private readonly int _posX;
+        private readonly int _posY;
+
+        public InlineProgress()
+        {
+            _posX = Console.CursorLeft;
+            _posY = Console.CursorTop;
+        }
+
+        public void Report(double progress)
+        {
+            Console.SetCursorPosition(_posX, _posY);
+            Console.WriteLine($"{progress:P0}");
+        }
+
+        public void Dispose()
+        {
+            Console.SetCursorPosition(_posX, _posY);
+            Console.WriteLine("Готово!\r\n");
+        }
+    }
+}
